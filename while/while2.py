5b8df651b128ab9359184d00fd450d0afeed868c
panggggg <p.picczy@gmail.com>
2021-02-06 12:01:45 +0700

    Add new folder and new 4 files

---
+++ while/while2.py
@@ -0,0 +1,10 @@
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+# break หยุดได้
+# 1
+# 2
+# 3
---
+++ while/while4.py
@@ -0,0 +1,10 @@
+i = 1
+while i < 3:
+    print(i)
+    i += 1
+else:
+    print("i is no longer than 3")
+
+# 1
+# 2
+# i is no longer than 3
