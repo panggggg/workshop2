cd711f7bdd7263ed7eb06683de420015adef9f0e
panggggg <p.picczy@gmail.com>
2021-02-20 11:56:58 +0700

    Add new Module folder

---
+++ module/function/calculate.py
@@ -0,0 +1,14 @@
+def add(number1, number2):
+    return number1 + number2
+
+
+def multiplied(number1, number2):
+    return number1 * number1
+
+
+def substract(number1, number2):
+    return number1 - number2
+
+
+def divide(number1, number2):
+    return number1 / number2
---
+++ module/variables/variables.py
@@ -0,0 +1,2 @@
+person = {"name": "Pang", "age": 21, "conutry": "Chonburi"}
+person1 = {"name": "BB", "age": 21, "country": "Chonburi"}
